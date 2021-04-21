ba7acef46c088166d440461c50e245cdacfc8872
wanderer06 <georgica_fara_de_frica@yahoo.com>
2021-04-21 16:21:44 +0100

    Working custom user draw

--- src/maho.rs
+++ src/maho.rs
@@ -6,12 +6,14 @@ use bindings::Windows::Win32::{
     Direct2D, DisplayDevices, Gdi, MenusAndResources, SystemServices, WindowsAndMessaging,
 };
 
-use windows::{Abi, Interface};
+use windows::{Abi, IUnknown, Interface};
 
 #[allow(dead_code)]
 pub struct Context {
+    factory: Option<Direct2D::ID2D1Factory1>,
     hwnd: WindowsAndMessaging::HWND,
-    hwnd_render_target: Direct2D::ID2D1HwndRenderTarget,
+    hwnd_render_target: Option<Direct2D::ID2D1HwndRenderTarget>,
+    brush: Option<Direct2D::ID2D1SolidColorBrush>,
 }
 
 pub enum Event {
@@ -23,15 +25,24 @@ pub enum Event {
 impl Context {
     pub fn create(title: &str, width: i32, height: i32) -> Self {
         unsafe {
+            let mut context = Self {
+                factory: None,
+                brush: None,
+                hwnd: WindowsAndMessaging::HWND(0),
+                hwnd_render_target: None,
+            };
+
             // obtain instance
             let h_instance = SystemServices::GetModuleHandleW(SystemServices::PWSTR::NULL);
             let h_instance = SystemServices::HINSTANCE(h_instance);
             let class_name = SystemServices::PWSTR::from(title);
 
+            debug_assert!(h_instance.0 != 0);
+
             // create window class
             let wnd_class = WindowsAndMessaging::WNDCLASSW {
                 style: WindowsAndMessaging::WNDCLASS_STYLES(0),
-                lpfnWndProc: Some(wnd_proc),
+                lpfnWndProc: Some(Context::wnd_proc),
                 cbClsExtra: 0,
                 cbWndExtra: 0,
                 hInstance: h_instance,
@@ -46,7 +57,7 @@ impl Context {
             WindowsAndMessaging::RegisterClassW(&wnd_class);
 
             // create window
-            let hwnd = WindowsAndMessaging::CreateWindowExW(
+            context.hwnd = WindowsAndMessaging::CreateWindowExW(
                 WindowsAndMessaging::WINDOW_EX_STYLE(0),
                 title,
                 title,
@@ -58,120 +69,83 @@ impl Context {
                 WindowsAndMessaging::HWND::NULL,
                 MenusAndResources::HMENU::NULL,
                 h_instance,
-                ::std::ptr::null_mut(),
+                &mut context as *mut Context as *mut libc::c_void, // pointer to additional context data
             );
 
             // show created window
             WindowsAndMessaging::ShowWindow(
-                hwnd,
+                context.hwnd,
                 WindowsAndMessaging::SHOW_WINDOW_CMD::SW_SHOWDEFAULT,
             );
 
-            Gdi::UpdateWindow(hwnd);
-
-            // get client rect
-            let mut rect = DisplayDevices::RECT::default();
-            WindowsAndMessaging::GetClientRect(hwnd, &mut rect);
-
-            // factory options
-            // include debug information
+            // create factory
             let mut options = Direct2D::D2D1_FACTORY_OPTIONS::default();
             options.debugLevel = Direct2D::D2D1_DEBUG_LEVEL::D2D1_DEBUG_LEVEL_INFORMATION;
-
-            let mut factory: Option<Direct2D::ID2D1Factory1> = None;
 
             Direct2D::D2D1CreateFactory(
                 Direct2D::D2D1_FACTORY_TYPE::D2D1_FACTORY_TYPE_SINGLE_THREADED,
                 &Direct2D::ID2D1Factory1::IID,
                 &options,
-                factory.set_abi(),
+                context.factory.set_abi(),
             )
             .unwrap();
 
-            let size = Direct2D::D2D_SIZE_U {
-                width: (rect.right - rect.left) as u32,
-                height: (rect.bottom - rect.top) as u32,
-            };
+            context
+        }
+    }
 
-            let mut hwnd_render_target: Option<Direct2D::ID2D1HwndRenderTarget> = None;
-            let render_target_properties = Direct2D::D2D1_RENDER_TARGET_PROPERTIES::default();
-            let hwnd_render_target_properties = Direct2D::D2D1_HWND_RENDER_TARGET_PROPERTIES {
-                hwnd,
-                pixelSize: size,
-                presentOptions: Direct2D::D2D1_PRESENT_OPTIONS::default(),
-            };
+    fn create_graphic_resources(&mut self) {
+        if self.hwnd_render_target.is_none() {
+            unsafe {
+                // get client rect
+                let mut rect = DisplayDevices::RECT::default();
+                WindowsAndMessaging::GetClientRect(self.hwnd, &mut rect);
 
-            factory
-                .unwrap()
-                .CreateHwndRenderTarget(
-                    &render_target_properties,
-                    &hwnd_render_target_properties,
-                    &mut hwnd_render_target,
-                )
-                .unwrap();
+                let size = Direct2D::D2D_SIZE_U {
+                    width: (rect.right - rect.left) as u32,
+                    height: (rect.bottom - rect.top) as u32,
+                };
 
-            let hwnd_render_target = hwnd_render_target.unwrap();
+                let render_target_properties = Direct2D::D2D1_RENDER_TARGET_PROPERTIES::default();
+                let hwnd_render_target_properties = Direct2D::D2D1_HWND_RENDER_TARGET_PROPERTIES {
+                    hwnd: self.hwnd,
+                    pixelSize: size,
+                    presentOptions: Direct2D::D2D1_PRESENT_OPTIONS::default(),
+                };
 
-            // let's try drawing something
-            let clear_colour = Direct2D::D2D1_COLOR_F {
-                r: 1.0,
-                g: 0.0,
-                b: 0.0,
-                a: 1.0,
-            };
+                self.factory.as_ref()
+                    .unwrap()
+                    .CreateHwndRenderTarget(
+                        &render_target_properties,
+                        &hwnd_render_target_properties,
+                        &mut self.hwnd_render_target,
+                    )
+                    .unwrap();
 
-            let brush_colour = Direct2D::D2D1_COLOR_F {
-                r: 1.0,
-                g: 0.0,
-                b: 1.0,
-                a: 1.0,
-            };
+                let brush_colour = Direct2D::D2D1_COLOR_F {
+                    r: 1.0,
+                    g: 0.0,
+                    b: 1.0,
+                    a: 1.0,
+                };
 
-            let render_target_size = hwnd_render_target.GetSize();
-            println!("Render target size: {:?}", render_target_size);
+                let brush_properties = Direct2D::D2D1_BRUSH_PROPERTIES {
+                    opacity: 1.0,
+                    transform: bindings::Windows::Foundation::Numerics::Matrix3x2::identity(),
+                };
 
-            let mut tag1: u64 = 0;
-            let mut tag2: u64 = 0;
-
-            let fill_rect = Direct2D::D2D_RECT_F {
-                left: render_target_size.width / 2.0 - 50.0,
-                top: render_target_size.height / 2.0 - 50.0,
-                right: render_target_size.width / 2.0 + 50.0,
-                bottom: render_target_size.height / 2.0 + 50.0,
-            };
-
-            let brush_properties = Direct2D::D2D1_BRUSH_PROPERTIES {
-                opacity: 1.0,
-                transform: bindings::Windows::Foundation::Numerics::Matrix3x2::identity(),
-            };
-
-            let mut brush: Option<Direct2D::ID2D1SolidColorBrush> = None;
-            hwnd_render_target
-                .CreateSolidColorBrush(&brush_colour, &brush_properties, &mut brush)
-                .unwrap();
-                
-            let brush = brush.unwrap();
-
-            println!("Created brush {:?}", brush);
-
-            let matrix_identity = bindings::Windows::Foundation::Numerics::Matrix3x2::identity();
-
-            hwnd_render_target.BeginDraw();
-            hwnd_render_target.Clear(&clear_colour);
-            hwnd_render_target.SetTransform(&matrix_identity);
-            hwnd_render_target.FillRectangle(&fill_rect, &brush);
-            // hwnd_render_target.Flush(&mut tag1, &mut tag2).unwrap();
-            hwnd_render_target.EndDraw(&mut tag1, &mut tag2).unwrap();
-
-            if tag1 != 0 || tag2 != 0 {
-                println!("Possible draw error, codes: {}, {}", tag1, tag2);
-            }
-
-            Self {
-                hwnd,
-                hwnd_render_target,
+                self.hwnd_render_target
+                    .as_ref()
+                    .unwrap()
+                    .CreateSolidColorBrush(&brush_colour, &brush_properties, &mut self.brush)
+                    .unwrap();
             }
         }
+    }
+
+    fn discard_graphic_resources(&mut self) {
+        self.brush = None;
+        self.hwnd_render_target = None;
     }
 
     pub fn get_event(&self) -> Event {
@@ -205,73 +179,112 @@ impl Context {
         Event::None
     }
 
-    // fn handle_wnd_proc_message(
-    //     &self,
-    //     msg: u32,
-    //     wparam: WindowsAndMessaging::WPARAM,
-    //     lparam: WindowsAndMessaging::LPARAM,
-    // ) -> SystemServices::LRESULT {
-    //     println!("handling own wnd proc msg");
-    // }
-}
+    fn test_render(&mut self) {
+        unsafe {
+            self.create_graphic_resources();
 
-#[no_mangle]
-extern "system" fn wnd_proc(
-    hwnd: WindowsAndMessaging::HWND,
-    msg: u32,
-    wparam: WindowsAndMessaging::WPARAM,
-    lparam: WindowsAndMessaging::LPARAM,
-) -> SystemServices::LRESULT {
-    unsafe {
-        return match msg {
-            WindowsAndMessaging::WM_PAINT => {
-                // self.test_render();
-                SystemServices::LRESULT(0)
+            let hwnd_render_target = self.hwnd_render_target.as_ref().unwrap();
+            let brush = self.brush.as_ref().unwrap();
+
+            // let's try drawing something
+            let clear_colour = Direct2D::D2D1_COLOR_F {
+                r: 1.0,
+                g: 0.0,
+                b: 0.0,
+                a: 1.0,
+            };
+
+            let render_target_size = hwnd_render_target.GetSize();
+
+            let fill_rect = Direct2D::D2D_RECT_F {
+                left: render_target_size.width / 2.0 - 50.0,
+                top: render_target_size.height / 2.0 - 50.0,
+                right: render_target_size.width / 2.0 + 50.0,
+                bottom: render_target_size.height / 2.0 + 50.0,
+            };
+
+            let mut ps = Gdi::PAINTSTRUCT::default();
+            Gdi::BeginPaint(&self.hwnd, &mut ps);
+
+            let matrix_identity = bindings::Windows::Foundation::Numerics::Matrix3x2::identity();
+
+            hwnd_render_target.BeginDraw();
+
+            hwnd_render_target.Clear(&clear_colour);
+            hwnd_render_target.SetTransform(&matrix_identity);
+            hwnd_render_target.FillRectangle(&fill_rect, brush);
+
+            let hr = hwnd_render_target.EndDraw(std::ptr::null_mut(), std::ptr::null_mut());
+            if hr.is_err() {
+                self.discard_graphic_resources();
             }
-            WindowsAndMessaging::WM_DESTROY => {
-                WindowsAndMessaging::PostQuitMessage(0);
-                SystemServices::LRESULT(0)
-            }
-            _ => WindowsAndMessaging::DefWindowProcW(hwnd, msg, wparam, lparam),
-        };
+
+            Gdi::EndPaint(&self.hwnd, &ps);
+        }
     }
 
-    // unsafe {
-    //     // println!("entered wnd_proc, msg is {} (need {} for WM_NCCREATE)", msg, WindowsAndMessaging::WM_NCCREATE);
-    //     // this is a bit tricky
-    //     // we need to store our main struct pointer
-    //     // so we can call it later
-    //     if msg == WindowsAndMessaging::WM_CREATE {
-    //         println!("inside wm_create");
-    //         // first time the window is created
-    //         // we have all the parameters used in CreateWindowEx
-    //         // The window procedure of the new window receives this message after the window is created, but before the window becomes visible
-    //         let pointer_create_struct = lparam.0 as *const WindowsAndMessaging::CREATESTRUCTW;
-    //         println!("pointer create struct app here: {:?}", *pointer_create_struct);
-    //         let pointer_app = (*pointer_create_struct).lpCreateParams as  *mut Context;
-    //         (*pointer_app).hwnd = hwnd;
+    fn handle_wnd_proc_message(
+        &mut self,
+        msg: u32,
+        wparam: WindowsAndMessaging::WPARAM,
+        lparam: WindowsAndMessaging::LPARAM,
+    ) -> SystemServices::LRESULT {
+        unsafe {
+            match msg {
+                WindowsAndMessaging::WM_PAINT => {
+                    self.test_render();
+                    SystemServices::LRESULT(0)
+                }
+                WindowsAndMessaging::WM_DESTROY => {
+                    self.discard_graphic_resources();
+                    WindowsAndMessaging::PostQuitMessage(0);
+                    SystemServices::LRESULT(0)
+                }
+                _ => WindowsAndMessaging::DefWindowProcW(self.hwnd, msg, wparam, lparam),
+            }
+        }
+    }
 
-    //         // change the long window pointer for the window to our own app pointer
-    //         WindowsAndMessaging::SetWindowLongW(
-    //             hwnd,
-    //             WindowsAndMessaging::WINDOW_LONG_PTR_INDEX::GWL_USERDATA,
-    //             pointer_app as i32,
-    //         );
-    //     } else {
-    //         println!("inside else wm_create");
-    //         // retrieve the app pointer
-    //         let pointer_app = WindowsAndMessaging::GetWindowLongW(
-    //             hwnd,
-    //             WindowsAndMessaging::WINDOW_LONG_PTR_INDEX::GWL_USERDATA,
-    //         ) as *mut Context;
+    #[no_mangle]
+    extern "system" fn wnd_proc(
+        hwnd: WindowsAndMessaging::HWND,
+        msg: u32,
+        wparam: WindowsAndMessaging::WPARAM,
+        lparam: WindowsAndMessaging::LPARAM,
+    ) -> SystemServices::LRESULT {
+        unsafe {
+            // this is a bit tricky
+            // we need to pull our Self reference from the create data
+            // so we can call it later from inside the window process
+            if msg == WindowsAndMessaging::WM_CREATE {
+                // first time the window is created
+                // we have all the parameters used in CreateWindowEx
+                // The window procedure of the new window receives this message after the window is created, but before the window becomes visible
+                let pointer_create_struct = lparam.0 as *const WindowsAndMessaging::CREATESTRUCTW;
+                let pointer_app = (*pointer_create_struct).lpCreateParams as *mut Context;
+                (*pointer_app).hwnd = hwnd;
 
-    //         // call a procedure on our app pointer
-    //         println!("calling handle proc on our class");
-    //         (*pointer_app).handle_wnd_proc_message(msg, wparam, lparam);
-    //     }
+                SetWindowLong(
+                    hwnd,
+                    WindowsAndMessaging::WINDOW_LONG_PTR_INDEX::GWLP_USERDATA,
+                    pointer_app as isize,
+                );
+            } else {
+                // retrieve the app pointer
+                let pointer_app = GetWindowLong(
+                    hwnd,
+                    WindowsAndMessaging::WINDOW_LONG_PTR_INDEX::GWLP_USERDATA,
+                ) as *mut Context;
 
-    //     WindowsAndMessaging::DefWindowProcW(hwnd, msg, wparam, lparam)
-    // }
+                if !pointer_app.is_null() {
+                    // call a procedure on our app pointer
+                    (*pointer_app).handle_wnd_proc_message(msg, wparam, lparam);
+                }
+            }
+
+            WindowsAndMessaging::DefWindowProcW(hwnd, msg, wparam, lparam)
+        }
+    }
 }
 
 impl SystemServices::PWSTR {
@@ -283,4 +296,44 @@ impl SystemServices::PWSTR {
                 .as_mut_ptr(),
         )
     }
+}
+
+// we need to call different WindowLong functions
+// since win32 will not have the definitions for win64 WindowLongPtr variants
+#[allow(non_snake_case)]
+#[cfg(target_pointer_width = "32")]
+unsafe fn SetWindowLong(
+    hwnd: WindowsAndMessaging::HWND,
+    nindex: WindowsAndMessaging::WINDOW_LONG_PTR_INDEX,
+    dwnewlong: isize,
+) -> isize {
+    WindowsAndMessaging::SetWindowLongW(hwnd, nindex, dwnewlong as isize) as isize
+}
+
+#[allow(non_snake_case)]
+#[cfg(target_pointer_width = "64")]
+unsafe fn SetWindowLong(
+    hwnd: WindowsAndMessaging::HWND,
+    nindex: WindowsAndMessaging::WINDOW_LONG_PTR_INDEX,
+    dwnewlong: isize,
+) -> isize {
+    WindowsAndMessaging::SetWindowLongPtrW(hwnd, nindex, dwnewlong)
+}
+
+#[allow(non_snake_case)]
+#[cfg(target_pointer_width = "32")]
+unsafe fn GetWindowLong(
+    hwnd: WindowsAndMessaging::HWND,
+    nindex: WindowsAndMessaging::WINDOW_LONG_PTR_INDEX,
+) -> isize {
+    WindowsAndMessaging::GetWindowLongW(hwnd, nindex) as isize
+}
+
+#[allow(non_snake_case)]
+#[cfg(target_pointer_width = "64")]
+unsafe fn GetWindowLong(
+    hwnd: WindowsAndMessaging::HWND,
+    nindex: WindowsAndMessaging::WINDOW_LONG_PTR_INDEX,
+) -> isize {
+    WindowsAndMessaging::GetWindowLongPtrW(hwnd, nindex)
 }
